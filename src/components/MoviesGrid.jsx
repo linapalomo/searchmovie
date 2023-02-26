b6283f56eb635ac5f7aefa728d5fec4af506f7fc
linapalomo <linapalomog@hotmail.com>
2023-02-26 16:54:10 +0100

    update-dependencies-install- react-icons

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
-import styles from "./MovieGrids.module.css";
+import {Spinner} from "./Spinner";
+import { useQuery } from "../hooks/useQuery";
+import styles from './MovieGrids.module.css';
 
 export function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const query = useQuery();
+  const search = query.get("search");
 
   useEffect(() => {
-    get("/discover/movie").then((data) => {
+    setIsLoading(true);
+    const searchUrl = search
+      ? "/search/movie?query=" + search
+      : "/discover/movie";
+    get(searchUrl).then((data) => {
       setMovies(data.results);
+      setIsLoading(false);
     });
-  }, []);
+  }, [search]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
