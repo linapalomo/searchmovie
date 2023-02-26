7dbb3ac0b25ce9de982e8a9a3a05a2772cfde2b3
linapalomo <linapalomog@hotmail.com>
2023-02-26 13:32:10 +0100

    changes to update the Switch -dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { MoviesGrid } from "./MoviesGrid";
-import styles from "./App.module.css"
+
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { MovieDetails }  from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+
 
 export function App() {
   return (
-    <div className="card">
-      <header  className={styles.title}>
-        <h1>Series and Movies finder</h1>
+    <Router>
+      <header>
+        <Link to="/">
+        <h1 className={styles.title}>Series and Movies finder</h1>
+        </Link>
       </header>
       <main>
-        <MoviesGrid />
+        <Routes>
+          <Route exact path="/movies/:movieId">
+            <MovieDetails/>
+          </Route>
+          <Route path="/">
+            <LandingPage/>
+          </Route>
+        </Routes>
+       
       </main>
-    </div>
+    </Router>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.css";
 
 export function MovieCard({movie}){
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
     return (
         <li className={styles.movieCard}>
+            <Link to= {"/movies/" + movie.id}>
             <img 
             width={230}
             height={345}
@@ -11,7 +13,7 @@ export function MovieCard({movie}){
             src={imageUrl}
             alt={movie.title}
             />
-            <div>{movie.title}</div>
+            <div>{movie.title}</div></Link>
         </li>
        
     );
